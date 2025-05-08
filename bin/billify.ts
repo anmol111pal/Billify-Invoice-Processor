dbad4490a92b28668e9202721aa1559ac230c178
Anmol Pal <anmol111pal@gmail.com>
2025-05-09 00:29:03 +0530

    Set deployment env with accout ID & region

--- bin/billify.ts
+++ bin/billify.ts
@@ -14,7 +14,10 @@ const billifyStack = new BillifyStack(app, 'BillifyStack', {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+  env: {
+    account: '637423439617',
+    region: 'ap-south-1',
+  },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
